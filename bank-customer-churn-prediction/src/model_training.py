cfa917b34bdfe5d01252f150bcdc6f7827b371f6
adithi741 <147181152+adithi741@users.noreply.github.com>
2025-01-09 21:47:16 +0530

    Update model_training.py

--- bank-customer-churn-prediction/src/model_training.py
+++ bank-customer-churn-prediction/src/model_training.py
@@ -1,1 +1,19 @@
+Model Training :
+"Model training is the process of iteratively adjusting a model's parameters to minimize the difference between its predictions and the actual values in the training data."
 
+=============================================================================================================
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(final_feature_set, labels, test_size = 0.25, random_state = 42)
+
+// from xgboost import XGBClassifier
+// import xgboost as xgb
+// from xgboost import DMatrix
+
+model = XGBClassifier(learning_rate =0.1, n_estimators=100 , random_state=42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print(classification_report(y_test,y_pred ))
+print(accuracy_score(y_test, y_pred))
